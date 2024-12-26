0c2cdc1e4d8b199a81b9b1db3c692b8274efa18d
sejung <sejungkim@Sejungs-iMac.local>
2024-12-25 20:55:07 -0500

    feat: finalize landing page layout and styling
    - Set left side to 55% width
    - Adjusted flex properties for responsiveness
    - Styled buttons for fill and stroke types

--- client/src/components/LoginPage/LoginPage.js
+++ client/src/components/LoginPage/LoginPage.js
@@ -6,17 +6,29 @@ const LoginPage = () => {
     <div className="login-container">
       <div className="left-side">
         <div className="left-content">
-          <h1 className="login-text">Interactive learning made easy: Start mastering ASL.</h1>
-          <img src="/images/asl-logo.svg" alt="Description" className="login-image" />
+          <h1 className="login-text">
+            Interactive learning made easy: Start mastering ASL.
+          </h1>
+          <img 
+            src="/images/asl-logo.svg" 
+            alt="ASL Logo" 
+            className="login-image" 
+          />
         </div>
       </div>
       <div className="right-side">
-      <button className="login-button get-started-button" onClick={() => alert("Get Started clicked")}>
-        Get Started
-      </button>
-      <button className="login-button log-in-button" onClick={() => alert("Log In clicked")}>
-        Log In
-      </button>
+        <button 
+          className="button stroke" 
+          onClick={() => alert("Get Started clicked")}
+        >
+          Get Started
+        </button>
+        <button 
+          className="button fill" 
+          onClick={() => alert("Log In clicked")}
+        >
+          Log In
+        </button>
       </div>
     </div>
   );
